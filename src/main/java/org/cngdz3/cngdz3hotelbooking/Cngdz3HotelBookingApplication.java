4388eb400cf833b5a307cd4214dd34fa3dee484e
Regecz <regecz.mark@gmail.com>
2025-05-23 20:05:23 +0200

    Initial commit

---
+++ src/main/java/org/cngdz3/cngdz3hotelbooking/Cngdz3HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package org.cngdz3.cngdz3hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cngdz3HotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cngdz3HotelBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cngdz3/cngdz3hotelbooking/Cngdz3HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cngdz3.cngdz3hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cngdz3HotelBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
